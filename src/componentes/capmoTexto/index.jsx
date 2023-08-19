ce4cc2c6f2eecbcbf927f3a8c86a21c2a5f7ba14
Betuelferreira <betobetuelferreira@gmail.com>
2023-08-18 23:55:05 -0300

    font input

--- src/componentes/capmoTexto/index.jsx
+++ src/componentes/capmoTexto/index.jsx
@@ -3,7 +3,7 @@ import "./CaixaTexto.css"
 function CampoTexto(props){
     return(
         <div id="divTexto">
-            <label>{props.label}</label>
+            <label id="lbl">{props.label}</label>
             <input id="inp" type="text" placeholder={props.placeholder} />
             
         </div>
