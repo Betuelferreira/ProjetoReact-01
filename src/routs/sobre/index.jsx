709b9fcaa30dee9548f5777a548cd6767626c1d5
Betuelferreira <betobetuelferreira@gmail.com>
2023-09-26 11:35:28 -0300

    api

--- src/routs/sobre/index.jsx
+++ src/routs/sobre/index.jsx
@@ -1,23 +1,47 @@
-import { useEffect } from "react"
+/* import { useEffect, useState } from "react"
 import Banner from "../../componentes/banner"
 import Footer from "../../componentes/footer"
 import Header from "../../componentes/header"
-
+import axios from "axios";
 
 
 export default function Sobre(){
-    useEffect(()=>{
-        window.scrollTo(0,0)
+    const[data, setData] = useState()
 
-    },[])
+const HandleFetch = async () =>{
+    await axios.get("https://randomuser.me/api/?results=20")
+    .then((response)=>{
+        setData(response.data.results)
+        console.log(data)
+    })
+
+    .catch((error)=>{
+        console.log(error)
+    })
+
+}
+
+useEffect(()=>{
+    HandleFetch()
+},[])
+
+    
+    
 
     return(
         <div>
             <Header/>
             <Banner/>
+
+            <div>{data.map((item)) => {
+                return(
+                    <p></p>
+                )
+            }
+            </div>
             <Footer/>
 
       
         </div>
     )
-}
+} */
